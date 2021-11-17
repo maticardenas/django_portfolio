108429c25f3a39ab25dbce9e07311346e8310553
Matias Cardenas <matias.cardenas@sendcloud.com>
2021-11-17 22:56:42 +0100

    chore: adding models and registering them at admin

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -15,6 +18,9 @@ class Post(models.Model):
         auto_now=True
     )  # auto_now sets current date and time everytime it is saved.
     categories = models.ManyToManyField("Category", related_name="posts")
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
