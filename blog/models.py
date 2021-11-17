18472f4d982785947c19e12b4c0be2b813014474
Matias Cardenas <matias.cardenas@sendcloud.com>
2021-11-17 17:09:34 +0100

    fix: fixing all files with black and isort

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
@@ -7,13 +8,17 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True) # auto_now_add sets current date everytime it is created.
-    last_modified = models.DateTimeField(auto_now=True) # auto_now sets current date and time everytime it is saved.
-    categories = models.ManyToManyField('Category', related_name='posts')
+    created_on = models.DateTimeField(
+        auto_now_add=True
+    )  # auto_now_add sets current date everytime it is created.
+    last_modified = models.DateTimeField(
+        auto_now=True
+    )  # auto_now sets current date and time everytime it is saved.
+    categories = models.ManyToManyField("Category", related_name="posts")
 
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
--- projects/views.py
+++ projects/views.py
@@ -1,22 +1,22 @@
 from typing import TYPE_CHECKING
+
 from django.shortcuts import render
+
 from projects.models import Project
 
 if TYPE_CHECKING:
-    from response import Response
     from urllib.request import Request
+
+    from response import Response
+
 
 def project_index(request: "Request") -> "Response":
     projects = Project.objects.all()
-    context = {
-        'projects': projects
-    }
-    return render(request, 'project_index.html', context)
+    context = {"projects": projects}
+    return render(request, "project_index.html", context)
 
 
 def project_detail(request: "Request", pk: int) -> "Response":
     project = Project.objects.get(pk=pk)
-    context = {
-        'project': project
-    }
-    return render(request, 'project_detail.html', context)
+    context = {"project": project}
+    return render(request, "project_detail.html", context)
