e6a17cb4cd4fa5972d11e4dbe46a9a69fedd41f4
Matias Cardenas <matias.cardenas@sendcloud.com>
2021-11-20 17:30:51 +0100

    feat: adding login / logout functionality

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
+from typing import TYPE_CHECKING
+
 from django.shortcuts import render
 
-# Create your views here.
+if TYPE_CHECKING:
+    from response import Response
+    from urllib.request import Request
+
+
+def dashboard(request: "Request") -> "Response":
+    return render(request, "dashboard.html")
